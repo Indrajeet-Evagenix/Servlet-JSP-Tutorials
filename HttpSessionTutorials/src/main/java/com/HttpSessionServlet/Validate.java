6aa5abc3453b220d1ff9120acfe7b53f68610ae1
Indrajeet Gupta <Indrajeet Gupta@DESKTOP-M85K85Q>
2023-08-15 12:48:50 +0530

    HttpSession

---
+++ HttpSessionTutorials/src/main/java/com/HttpSessionServlet/Validate.java
@@ -0,0 +1,43 @@
+package com.HttpSessionServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Validate
+ */
+public class Validate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Validate() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("user");
+		String pass = request.getParameter("pass");
+
+		HttpSession session = request.getSession();
+
+		if (name.equals("admin") && pass.equals("admin")) {
+			session.setAttribute("user", name);
+			response.sendRedirect("Welcome");
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+}
